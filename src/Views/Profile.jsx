956345c3db70368a18ec664ee4c79cad2d5fb2e5
MepexDash <100921931+MepexDash@users.noreply.github.com>
2023-01-17 16:53:17 +0100

    until 8:30 vid 14

--- src/Views/Profile.jsx
+++ src/Views/Profile.jsx
@@ -1,18 +1,32 @@
+import { useEffect } from "react"
+import { userById } from "../api/user"
 import ProfileActions from "../Components/Profile/ProfileActions"
 import ProfileHeader from "../Components/Profile/ProfileHeader"
 import ProfileOrderHistory from "../Components/Profile/ProfileOrderHistory"
+import { STORAGE_KEY_USER } from "../const/storageKeys"
 import { useUser } from "../context/UserContext"
 import withAuth from "../hoc/withAuth"
+import { storageSave } from "../utils/storage"
 
 const Profile = () => {
-    const { user } = useUser()
-     
+    const { user, setUser } = useUser()
+
+    useEffect(() => { //useEffect can't be asyncronous 
+        const findUser = async () => {
+            const [error, latestUser] = await userById(user.id)
+            if (error === null) {
+                storageSave(STORAGE_KEY_USER, latestUser)
+                setUser(latestUser)
+            }
+        }
+        findUser()
+    }, [ setUser, user.id ])
     return (
         <>
             <h1>Profile</h1>
-            <ProfileHeader username={ user.username }/>
-            <ProfileActions/>
-            <ProfileOrderHistory orders={ user.orders }/>
+            <ProfileHeader username={user.username} />
+            <ProfileActions />
+            <ProfileOrderHistory orders={user.orders} />
         </>
     )
 }
--- src/api/user.js
+++ src/api/user.js
@@ -57,5 +57,17 @@ export const loginUser = async (username)  => {
     
 }
 
+export const userById = async (userId) => {
+    try {
+        const response = await fetch(`${apiUrl}/${userId}`)
+        if (!response.ok) {
+            throw new Error('Could not fetch')
+        }
+        const user = await response.json()
+        return [ null, user ]
+    }
 
-
+    catch (error) {
+        return [ error.message, null ]
+    }
+}
